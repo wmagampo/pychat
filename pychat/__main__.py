2f710f8b3313ff49f76f2470a8ea277649a61f08
Will <willymagampo@gmail.com>
2022-12-19 12:31:58 +0800

    2nd try

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -7,6 +7,7 @@ def main():
     completion = openai.Completion.create(engine="text-davinci-003", prompt=query, max_tokens=4000)
     output = completion.choices[0].text
     print("Output: {}".format(output))
+    print("Done!")
 
 
 if __name__== '__main__':
